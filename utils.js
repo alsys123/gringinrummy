19a9aefbe9087675c2565f8cc397f5a641fd00fd
alsys123 <alsys123@gmail.com>
2026-02-12 17:07:49 -0500

    small change to stats - v2

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@
      General Backdoor
   ------------------------------ */
 
-const gBackDoorLock = 1;  // when to open/close back door .. 3 is good
+const gBackDoorLock = 3;  // when to open/close back door .. 3 is good
 let gBackDoorCode = 0;
 let gBackDoorDir = 1; // +1 going up, -1 going down ... the direction
 
